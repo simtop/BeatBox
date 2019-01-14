94bf8c4f39f3cad8443eadd5d1b05f9df4c28ccb
simtop <41802904+simtop@users.noreply.github.com>
2019-01-14 21:58:57 +0100

    First steps of data binding, to show sample_sounds names on the recyclerview and update list wile scrolling

--- app/src/main/java/com/example/simon/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/example/simon/beatbox/BeatBoxActivity.java
@@ -1,13 +1,15 @@
 package com.example.simon.beatbox;
 
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
---
+++ app/src/main/java/com/example/simon/beatbox/SoundViewModel.java
@@ -0,0 +1,30 @@
+package com.example.simon.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+
+    }
+
+}
